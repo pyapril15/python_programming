403f07e9aed2a88551ad2c1595a81c5a4688c305
Praveen Yadav <65152549+pyapril15@users.noreply.github.com>
2024-12-06 20:53:12 +0530

    recursion power

---
+++ recursion/power.py
@@ -0,0 +1,10 @@
+def power(base, exp):
+    if exp == 0:
+        return 1
+
+    return  base * power(base, exp - 1)
+
+base = int(input("Enter the base number: "))
+exp = int(input("Enter the exp number:- "))
+
+print(power(base, exp))
